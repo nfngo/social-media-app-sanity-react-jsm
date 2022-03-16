50b2f54d04f8e083c0b65f7f343983cc0af23dc5
Nuno Oliveira <nfnoliveira@hotmail.com>
2022-03-16 16:31:38 +0000

    Update Pin component, fix Home container  and Sidebar component small bugs, add utils function fetchUser

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -76,6 +76,7 @@ const Sidebar = ({ user, closeToggle }) => {
             alt="user-profile"
           />
           <p>{user.userName}</p>
+          <IoIosArrowForward />
         </Link>
       )}
     </div>
